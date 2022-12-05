e5ee59541e9f8ff2bd191607695bec8fd8e5bb20
busshi <busshi973@icloud.com>
2022-12-05 14:07:43 +0100

    constants

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -6,14 +6,14 @@ Bonjour ${firstName},
 
 Bienvenue dans mon réseau.
 
-En attendant de me parler directement, vous pouvez demander à mon bot quelques informations basiques à l'aide des commandes suivantes :
+En attendant de me parler directement, vous pouvez demander à mon bot (expérimental) quelques informations basiques à l'aide des commandes suivantes :
 
 - profile : pour en savoir un peu plus sur moi,
 - techno : pour connaître mes technologies préférées,
 - dispo : pour connaître mes disponibilités,
-- contact : pour communiquer directement avec moi et désactiver le bot,
+- contact : pour communiquer directement avec moi,
 
-Pour information, ce bot tourne sur mon raspberry et il manque parfois de ressources pour vous répondre ! Un peu de patience...
+Pour information, ce bot tourne sur mon raspberry (uniquement la journée) et il manque parfois de ressources pour vous répondre ! Un peu de patience...
 
 A très vite.
 
